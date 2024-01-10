98cae463e0ecf736dcff321e9eff3821ebe60f27
mofJon <jon.couch@matterofform.com>
2024-01-10 11:37:44 +0000

    added mouse interaction

--- src/components/Three/models/ZahaCyclone.tsx
+++ src/components/Three/models/ZahaCyclone.tsx
@@ -6,26 +6,21 @@ import { useSpring } from "framer-motion";
 
 const glb = "assets/3D/zhaCyclone2.glb";
 
-// let speed: number = 0;
-let mouseInterval: any;
-let pulse = 0;
-let isMouseMoving = false;
-
 const ZahaCyclone = (props: any) => {
-  // const [speed, setSpeed] = useState(0)
+  const glassMat = useRef<any>();
   const glassRef = useRef<any>();
-  // const thick = useSpring(speed);
+  const pulse = useSpring(0, { damping: 30, stiffness: 100 });
 
   const { glassSize, glassZ, rotationX, rotationY, rotationZ } = useControls({
     glassSize: { value: 0.4, min: 0, max: 10 },
-    glassZ: { value: 0.24, min: -50, max: 20 },
+    glassZ: { value: -0.5, min: -50, max: 20 },
     rotationX: { value: 0, min: -Math.PI, max: Math.PI },
     rotationY: { value: 0, min: -Math.PI, max: Math.PI },
     rotationZ: { value: -0.3, min: -Math.PI, max: Math.PI },
   });
 
   const glassOptions = useControls({
-    transmission: { value: 2, min: 0, max: 100 },
+    // transmission: { value: 2, min: 0, max: 100 },
     backsideThickness: { value: 5, min: 0, max: 100 },
     roughness: { value: 0, min: 0, max: 1 },
     chromaticAberration: { value: 0.22, min: 0, max: 1 },
@@ -33,7 +28,7 @@ const ZahaCyclone = (props: any) => {
     distortion: { value: 0.1, min: 0, max: 10 },
     distortionScale: { value: 0.2, min: 0, max: 10 },
     temporalDistortion: { value: 0.26, min: 0, max: 1 },
-    transmissionSampler: { value: false },
+    // transmissionSampler: { value: false },
     backside: { value: false },
     // resolution: { value: undefined, min: 0, max: 10},
     // backsideResolution: { value: undefined, min: 0, max: 20},
@@ -41,51 +36,36 @@ const ZahaCyclone = (props: any) => {
     // background: { value: bgTex},
   });
 
-  const [{ thickness, wireframeMesh }, set] = useControls(() => ({
-    thickness: { value: 0.5, min: 0, max: 5 },
+  const [{ thickness, transmission, wireframeMesh }, set] = useControls(() => ({
+    thickness: { value: 0, min: 0, max: 5 },
+    transmission: { value: 2, min: 0, max: 100 },
     wireframeMesh: { value: false },
   }));
 
   const getMouseSpeed = (e: any) => {
-    const speed = (Math.abs(e.movementX) + Math.abs(e.movementY)) * 0.05;
-
-    // setSpeed(speed)
-    // set({thickness: speed});
-    isMouseMoving = true;
-
-    clearTimeout(mouseInterval);
-    mouseInterval = setTimeout(() => {
-      // setSpeed(0)
-      // set({thickness: speed});
-      isMouseMoving = false;
-    }, 300);
-
-    console.log(speed, "mouseMoving");
+    const speed = (Math.abs(e.movementX) + Math.abs(e.movementY)) * 0.08;
+    pulse.set(speed);
   };
 
   useEffect(() => {
     document.addEventListener("mousemove", getMouseSpeed);
   }, []);
 
-  // useEffect(() => {
-  //   thick.set(speed)
-  // },[speed])
-
   const { nodes }: any = useGLTF(glb);
 
   useFrame(() => {
-    if (isMouseMoving) {
-      pulse += 0.1;
-    } else if (isMouseMoving === false && pulse > 0) {
-      pulse -= 0.1;
-    }
-
     if (glassRef.current) {
       glassRef.current.rotation.y -= 0.0005;
     }
-  });
 
-  console.log(pulse, isMouseMoving);
+    if (glassMat.current) {
+      glassMat.current.thickness = (pulse as any).current + thickness;
+      glassMat.current._transmission =
+        (pulse as any).current * 0.5 + transmission;
+    }
+
+    console.log(glassMat.current, transmission);
+  });
 
   return (
     <group
@@ -103,10 +83,11 @@ const ZahaCyclone = (props: any) => {
          */}
       <mesh geometry={nodes.Cyclone.geometry}>
         <MeshTransmissionMaterial
+          ref={glassMat}
           attach="material"
           {...glassOptions}
-          thickness={thickness}
           wireframe={wireframeMesh}
+          needsUpdate
         />
       </mesh>
     </group>
